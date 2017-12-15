cf577d6833b675c2a48341012ad7ec0b57ab49fc
mohl <mohl@protonmail.com>
2017-12-15 10:37:45 +0100

    Add Phone service

---
+++ MVVM/MVVM/MVVM/Services/PhoneService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MVVM.Models;
+
+namespace MVVM.Services
+{
+    public class PhoneService
+    {
+
+        public List<Phone> GetPhones()
+        {
+            var list = new List<Phone>
+            {
+                new Phone
+                {
+                    Model = "iPhone X",
+                    Brand = "Apple"
+                },
+                new Phone
+                {
+                    Model = "Galaxy S7",
+                    Brand = "Samsung"
+                }
+            };
+
+            return list;
+        }
+
+    }
+}
