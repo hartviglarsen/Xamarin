0640481734ebf46067675bf9a72cdc368a68a2e9
mohl <mohl@protonmail.com>
2017-12-15 10:38:05 +0100

    Add Phone viewmodel

---
+++ MVVM/MVVM/MVVM/ViewModels/PhoneViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+using MVVM.Models;
+using MVVM.Services;
+
+namespace MVVM.ViewModels
+{
+    public class PhoneViewModel : INotifyPropertyChanged
+    {
+        private List<Phone> _phonesList;
+
+        public List<Phone> PhonesList
+        {
+            get => _phonesList;
+            set
+            {
+                _phonesList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public PhoneViewModel()
+        {
+            var phoneService = new PhoneService();
+            PhonesList = phoneService.GetPhones();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
