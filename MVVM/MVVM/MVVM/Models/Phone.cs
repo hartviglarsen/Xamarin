d9b730500c60911c07e3791a9703c65c57f9e09b
mohl <mohl@protonmail.com>
2017-12-15 10:37:28 +0100

    Add Phone model

---
+++ MVVM/MVVM/MVVM/Models/Phone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVM.Models
+{
+    public class Phone
+    {
+        public string Model { get; set; }
+        public string Brand { get; set; }
+    }
+}
