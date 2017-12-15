c360023e85474ce64c47c6b8159d7632b319672a
mohl <mohl@protonmail.com>
2017-12-15 13:40:01 +0100

    Add CustomListView
    For ItemTapped event

---
+++ MVVM/MVVM/MVVM/CustomListView.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MVVM
+{
+    public class CustomListView : ListView
+    {
+
+        public static BindableProperty ItemClickCommandProperty = 
+            BindableProperty.Create(
+                nameof(ItemClickCommand), 
+                typeof(ICommand), 
+                typeof(CustomListView), 
+                null
+         );
+
+        public ICommand ItemClickCommand
+        {
+            get
+            {
+                return (ICommand)this.GetValue(ItemClickCommandProperty);
+            }
+            set
+            {
+                this.SetValue(ItemClickCommandProperty, value);
+            }
+        }
+
+        public CustomListView()
+        {
+            this.ItemTapped += OnItenTapped;
+        }
+
+        private void OnItenTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item != null)
+            {
+                ItemClickCommand?.Execute(e.Item);
+                SelectedItem = null;
+            }
+        }
+    }
+}
