6f236404a9c47246a52d14cd94ee97dce3245863
mohl <mohl@protonmail.com>
2017-12-15 13:39:00 +0100

    Update Phone model
    Implement INotifyPropertyChanged

--- MVVM/MVVM/MVVM/Models/Phone.cs
+++ MVVM/MVVM/MVVM/Models/Phone.cs
@@ -1,13 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace MVVM.Models
 {
-    public class Phone
+    public class Phone : INotifyPropertyChanged
     {
-        public int ID       { get; set; }
-        public string Model { get; set; }
-        public string Brand { get; set; }
+
+        private int _id;
+        public int ID
+        {
+            get => _id;
+            set
+            {
+                _id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _model;
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                _model = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _brand;
+        public string Brand
+        {
+            get => _brand;
+            set
+            {
+                _brand = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private int _tapped;
+        public int Tapped
+        {
+            get => _tapped;
+            set
+            {
+                _tapped = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
